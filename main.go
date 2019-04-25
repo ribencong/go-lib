318c5e13f3cfc9777c3b4a2f91a0f4dd28ea827f
W-B-S <lws99927800@163.com>
2019-04-25 10:38:05 +0800

    time format

--- main.go
+++ main.go
@@ -8,18 +8,18 @@ import (
 )
 
 var conf = &client.Config{
-	Addr:        "YP7Bdx1LixC9yBnnmoJky4E4QsKxjCdhjvKfF64JxjRJfR",
-	Cipher:      "2C9fRZqk3SE73w7WEsBnodCCg4yNipv9pJEgyWEjG9ZKWnXUDwD1XfSLtmo9sLSy1sdHMpvvQ2KFgqQjYWKJSUJRM75hoWvaYr34suqdMP1kzP",
+	Addr:        "YPDsDm5RBqhA14dgRUGMjE4SVq7A3AzZ4MqEFFL3eZkhjZ",
+	Cipher:      "GffT4JanGFefAj4isFLYbodKmxzkJt9HYTQTKquueV8mypm3oSicBZ37paYPnDscQ7XoPa4Qgse6q4yv5D2bLPureawFWhicvZC5WqmFp9CGE",
 	LocalServer: ":51080",
-	License:     `{"sig":"VF2XU6t/gXSp1rSnMUMlYrvRJ3KMAmDHKmzP9ZkaM8rOb97CAdksr49bSe7uQ40mS7yxpyM+RxzmoWWlbhLhCQ==","start":"2019-04-24 09:04:00","end":"2019-05-04 09:04:00","user":"YP7Bdx1LixC9yBnnmoJky4E4QsKxjCdhjvKfF64JxjRJfR"}`,
+	SettingUrl:  "https://raw.githubusercontent.com/ribencong/ypctorrent/master/ypc_debug.torrent",
+	License:     `{"sig":"vQlEcc5XKX7B2Qxtwln4B6oijiUEUnI1DlI30hQEhELW1IUpVFvr2kTDunOrD2tWn39WagM3gk4trBx+jq5kAA==","start":"2019-04-25 09:09:54","end":"2019-05-05 09:09:54","user":"YPDsDm5RBqhA14dgRUGMjE4SVq7A3AzZ4MqEFFL3eZkhjZ"}`,
 }
 
 func main() {
 	clientMain()
 }
 func test1() {
-	LibVerifyLicense(`{"sig":"dzs4In4HGYcgZbAp0cskbh8gvCjZDNcqOTBOHN6+3DSiqZiUYk4Mb4g2CoIBvBJojSTh7JdUNPpp8fPbMwRtAQ==","start":"2019-04-23 08:34:21","end":"2019-05-03 08:34:21","user":"YPDsDm5RBqhA14dgRUGMjE4SVq7A3AzZ4MqEFFL3eZkhjZ"}
-`)
+	LibVerifyLicense(`{"sig":"vQlEcc5XKX7B2Qxtwln4B6oijiUEUnI1DlI30hQEhELW1IUpVFvr2kTDunOrD2tWn39WagM3gk4trBx+jq5kAA==","start":"2019-04-25 09:09:54","end":"2019-05-05 09:09:54","user":"YPDsDm5RBqhA14dgRUGMjE4SVq7A3AzZ4MqEFFL3eZkhjZ"}`)
 }
 
 func clientMain() {
