3381f8139b715b4f8cc2014c9a861afc728bb6bf
ribencong <ribencong@outlook.com>
2019-05-24 22:20:46 +0800

    still in debug process

--- ios/interface.go
+++ ios/interface.go
@@ -1,74 +1,83 @@
 package iosDelegate
 
 import (
-	"fmt"
-	"github.com/ribencong/go-lib/pipeProxy"
-	"github.com/ribencong/go-lib/tun2Pipe"
-	"github.com/ribencong/go-lib/wallet"
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/layers"
 )
 
-type VpnDelegate interface {
-	tun2Pipe.VpnDelegate
-	GetBootPath() string
+func ParseNsData(data []byte) string {
+	packet := gopacket.NewPacket(data, layers.LayerTypeIPv4, gopacket.Default)
+	if ip4Layer := packet.Layer(layers.LayerTypeIPv4); ip4Layer != nil {
+		ip4 := ip4Layer.(*layers.IPv4)
+		println(ip4.DstIP, ip4.SrcIP)
+		return packet.Dump()
+	}
+	return "---=>:unknown"
 }
 
-var _instance *pipeProxy.PipeProxy = nil
-var proxyConf = &pipeProxy.ProxyConfig{}
-
-func InitVPN(addr, cipher, license, url, boot, IPs string, d VpnDelegate) error {
-
-	pt := func(fd uintptr) {
-		d.ByPass(int32(fd))
-	}
-
-	proxyConf.WConfig = &wallet.WConfig{
-		BCAddr:     addr,
-		Cipher:     cipher,
-		License:    license,
-		SettingUrl: url,
-		Saver:      pt,
-	}
-
-	proxyConf.BootNodes = boot
-	tun2Pipe.ByPassInst().Load(IPs)
-
-	mis := proxyConf.FindBootServers(d.GetBootPath())
-	if len(mis) == 0 {
-		return fmt.Errorf("no valid boot strap node")
-	}
-
-	proxyConf.ServerId = mis[0]
-
-	tun2Pipe.VpnInstance = d
-	tun2Pipe.Protector = pt
-
-	return nil
-}
-
-func SetupVpn(password, locAddr string) error {
-
-	t2s, err := tun2Pipe.New(locAddr)
-	if err != nil {
-		return err
-	}
-
-	w, err := wallet.NewWallet(proxyConf.WConfig, password)
-	if err != nil {
-		return err
-	}
-
-	proxy, e := pipeProxy.NewProxy(locAddr, w, t2s)
-	if e != nil {
-		return e
-	}
-	_instance = proxy
-	return nil
-}
-
-func Run() {
-	_instance.Proxying()
-}
-
-func StopVpn() {
-	_instance.Close()
-}
+//
+//type VpnDelegate interface {
+//	tun2Pipe.VpnDelegate
+//	GetBootPath() string
+//}
+//
+//var _instance *pipeProxy.PipeProxy = nil
+//var proxyConf = &pipeProxy.ProxyConfig{}
+//
+//func InitVPN(addr, cipher, license, url, boot, IPs string, d VpnDelegate) error {
+//
+//	pt := func(fd uintptr) {
+//		d.ByPass(int32(fd))
+//	}
+//
+//	proxyConf.WConfig = &wallet.WConfig{
+//		BCAddr:     addr,
+//		Cipher:     cipher,
+//		License:    license,
+//		SettingUrl: url,
+//		Saver:      pt,
+//	}
+//
+//	proxyConf.BootNodes = boot
+//	tun2Pipe.ByPassInst().Load(IPs)
+//
+//	mis := proxyConf.FindBootServers(d.GetBootPath())
+//	if len(mis) == 0 {
+//		return fmt.Errorf("no valid boot strap node")
+//	}
+//
+//	proxyConf.ServerId = mis[0]
+//
+//	tun2Pipe.VpnInstance = d
+//	tun2Pipe.Protector = pt
+//
+//	return nil
+//}
+//
+//func SetupVpn(password, locAddr string) error {
+//
+//	t2s, err := tun2Pipe.New(locAddr)
+//	if err != nil {
+//		return err
+//	}
+//
+//	w, err := wallet.NewWallet(proxyConf.WConfig, password)
+//	if err != nil {
+//		return err
+//	}
+//
+//	proxy, e := pipeProxy.NewProxy(locAddr, w, t2s)
+//	if e != nil {
+//		return e
+//	}
+//	_instance = proxy
+//	return nil
+//}
+//
+//func Run() {
+//	_instance.Proxying()
+//}
+//
+//func StopVpn() {
+//	_instance.Close()
+//}
